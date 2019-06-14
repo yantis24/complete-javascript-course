02411dc74be0eae54350bd096284738db6a36161
Yanti Suryati <yantisc@gmail.com>
2019-06-14 12:11:48 +0100

    recap basic operator

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,7 +1,30 @@
+/*
 const firstname = 'Toby';
 console.log(firstname);
 
 const lastname = "cutey";
 console.log(lastname);
+*/
 
+let year, YearJohn, YearMark;
+now = 2019;
+JohnAge = 23;
+MarkAge = 35;
 
+//Math Operator//
+
+YearJohn = now - JohnAge;
+YearMark = now - MarkAge;
+
+console.log(YearMark);
+console.log(YearJohn);
+console.log(now + 10);
+
+//Logical Operator//
+const ageOlder = JohnAge > MarkAge;
+console.log (ageOlder);
+
+//Type of Operators//
+console.log(typeof ageOlder);
+console.log(typeof now);
+console.log(typeof "who is older?");
