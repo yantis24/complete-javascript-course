5acbef1b9d428b0baecd39d7fc11ff0c75b7be00
Yanti Suryati <yantisc@gmail.com>
2019-06-14 18:39:27 +0100

    ternary and switch operator exersices

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -36,14 +36,11 @@ console.log(lastname);
 // const x,y;
 // x = y = ( 3 + 5 )
 
-/*** CODING CHALLENGE 1
+/*** CODING CHALLENGE 1 ***********/
 
 /* Mark and John aree trying to compare ther BMI, which is calculated using
 the formula : BMI = mass /height^2 = mass / (height * height)
 (mass in kg and height in meter).
-
-
-
 */
 
 //1 store Mark's and John's mass and height in variables
@@ -55,6 +52,7 @@ const JohnWeight = 77;
 //2. calculate both their BMIs
  const MarkBMI = MarkWeight / (MarkHeight * 2);
  const JohnBMI = JohnWeight / (JohnHeight * 2);
+ console.log(MarkBMI, JohnBMI);
 
 //3. Create a boolean variable containing information about whether
 // Mark has a higher BMI than JOhn
@@ -64,6 +62,79 @@ const idealBMI = MarkBMI < JohnBMI ;
 //4. Print a string to the console containing the variable from step 3.
 //(somethingg like "Is Mark's BMI higher than John's true")
 console.log ( "is Mark's BMI higher than John's" + " "+ (idealBMI));
+
+/**** if/else statements *******/
+  if (MarkBMI > JohnBMI) {
+      console.log ('Mark\s BMI is Higher than John.');
+    }
+    else {
+      console.log ('John\s BMI is Higher than Mark.');
+    }
+/************************/
+/***** Boolean Logic*********/
+
+const firstname = 'Jane';
+const age = 23;
+
+  if (  age <= 13 ) {
+    console.log(firstname + ' still a girl!');
+    }
+    else if ( age > 13 && age <= 18) {
+    console.log (firstname + ' is a teenager');
+    }
+    else if ( age > 18 && age <= 30 ) {
+    console.log (firstname + ' is a young adult');
+    }
+    else {
+    console.log ( firstname + ' is a Lady!');
+    }
+
+/********************************/
+/********* Ternary Opeerator *********/
+
+const drink = age >= 18 ? 'Booze' : 'non-alcoholic';
+    console.log (drink);
+
+/******* Switch Operations********/
+
+const Job = 'Doctor';
+switch (Job) {
+      case 'Doctor':
+            console.log(firstname + ' works in hospital');
+            break;
+      case 'Teacher':
+            console.log(firstname + ' works at school');
+            break;
+      case 'Driver':
+            console.log(firstname + ' works for Uber');
+            break;
+      case 'Web developer':
+            console.log(firstname + ' works at Googgle');
+            break;
+      default:
+            console.log(firstname + ' works somewhere');
+}
+
+/*****************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
